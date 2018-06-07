dfaa859e91e59d001dfe84664b7a7d97e7c49451
iltenma <36898842+iltenma@users.noreply.github.com>
2018-06-07 10:57:59 +0200

    Add files via upload

---
+++ practica_17abril.ino
@@ -0,0 +1,19 @@
+int motor = 0;
+int pot = 0;
+
+void setup() {
+pinMode (10, OUTPUT);
+Serial.begin (9600);
+}
+
+void loop() {
+  motor = analogRead (0);
+  motor = map (motor, 1, 1023, 1, 255);
+  Serial.println (pot);
+  
+//analogWrite (pot);
+delay (1000);
+//analogWrite (pot);
+//delay (1000);
+
+}
